c0e80e47271ac90e9cfac297877b81dccb0e4abe
KritimPrasadKafle <kritim10k@gmail.com>
2025-03-20 22:23:28 +0545

    Fixed changes

--- scripts/deploySkillCoin.ts
+++ scripts/deploySkillCoin.ts
@@ -4,7 +4,7 @@ async function main() {
   const TalentCoin = await ethers.getContractFactory("TalentCoin");
   const talentCoin = await TalentCoin.deploy();
   await talentCoin.waitForDeployment();
-  console.log("TalentCoin deployed to:", await talentCoin.getAddress());
+  console.log("TalentCoin deploy to:", await talentCoin.getAddress());
 }
 
 main().catch((error) => {
